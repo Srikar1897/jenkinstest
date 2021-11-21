aeb448fa52a763f9d5a93275791d0544f50decda
Srikar1897 <67907622+Srikar1897@users.noreply.github.com>
2021-11-21 16:35:33 +0000

    Update newvars.tf

--- newvars.tf
+++ newvars.tf
@@ -1,6 +1,8 @@
 variable "AWS_ACCESS_KEY" {
+  default = "ASIASSZ2TSRJC26ZEEGC"
 }
 variable "AWS_SECRET_KEY" {
+  default = "6HERmw1JrehoBUQa4ihl8nVcIgt1Ex4QZfTrxeSh"
 }
 variable "AWS_TOKEN" {
   default = "FwoGZXIvYXdzEEUaDPX2guNoGH/Nbr5ekSLAAeG5LaCauh48isWwVnLwl1o87o5n6Wt6+C8zfAUgUMvfFnC16az+Fdi9yAJAIiOUj7eGzXO8HXquguadl+pFWebNcluawvwCL6kZkGW+SfPaUAmpxgWkFqXzm2qe5O38ETft0uEuuaSUn7930iAw/GolPX/81h6uE9COAKmHR0A9/hA5sQYYa9WXnimunkH5dC2e9S//51KN+cI0OwgT4QqTDnt+jAdgubq095myDEQx21r78NF5SCn9pR5nwXt/XijKweOMBjItp7hu3tRgBB/CwRjKlYPX25ww+HQJN7XBi7LHR+8xzYqCWNRQnMLd9EqqvHnk"
